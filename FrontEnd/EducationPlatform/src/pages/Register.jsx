da7ed83f81fc3be61bc79bca97fa2bd91b710346
dev-ramadan <ganna123adam@gmail.com>
2026-05-17 21:09:03 +0300

    TEST deploy

--- FrontEnd/EducationPlatform/src/pages/Register.jsx
+++ FrontEnd/EducationPlatform/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-
+import { BASE_URL } from "../api/config";
 export default function Register() {
   const navigate = useNavigate();
   const [form, setForm] = useState({
