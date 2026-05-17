c261f6c17a60de3ef23941959ce2c608c7802582
dev-ramadan <ganna123adam@gmail.com>
2026-05-17 17:32:30 +0300

    TEST

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,16 +1,36 @@
-import express from "express"
+// import express from "express"
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import bootstrap from "./src/app.js"
+
+// dotenv.config();
+
+// const app = express()
+// app.use(cors());
+// const port = process.env.PORT || 3000
+// bootstrap(app, express)
+// app.listen(port , ()=>{
+//     console.log("server is running on port " , port);
+// });
+
+// export default app;
+
+
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import bootstrap from "./src/app.js"
+import serverless from "serverless-http";
+import bootstrap from "./src/app.js";
 
 dotenv.config();
 
-const app = express()
+const app = express();
 app.use(cors());
-const port = process.env.PORT || 3000
-bootstrap(app, express)
-app.listen(port , ()=>{
-    console.log("server is running on port " , port);
-});
 
-export default app;
+// 👇 مهم نحط await
+await bootstrap(app, express);
+
+// ❌ مفيش app.listen
+
+// ✅ ده اللي Vercel بيستخدمه
+export default serverless(app);
