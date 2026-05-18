48f5ba1f2b3b8b870aa555daf1a2ad732dffb334
dev-ramadan <ganna123adam@gmail.com>
2026-05-18 23:31:16 +0300

    Fiex Cors Error

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,10 +1,26 @@
 import express from "express"
 import cors from "cors";
 import bootstrap from "./src/app.js"
+
 const app = express()
-app.use(cors());
-const port = process.env.PORT || 3000
+
+app.use(cors({
+    origin: [
+        "https://edu-plat-form.vercel.app",
+        "http://localhost:5173"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
+app.options('*', cors()); // مهم جدًا للـ preflight
+
+app.use(express.json()); // مهم لو بتبعت JSON
+
 bootstrap(app, express)
+
+const port = process.env.PORT || 3000
+
 app.listen(port, "0.0.0.0", () => {
     console.log("server is running on port", port);
 });
